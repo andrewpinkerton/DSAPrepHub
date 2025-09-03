9c2f1ef3da85e91136bd8a9c283eabb60318a2bc
Andrew Pinkerton <andrewpinkerton4@gmail.com>
2025-09-02 21:59:22 -0500

    ui update

--- scripts/problems.js
+++ scripts/problems.js
@@ -1,29 +1,36 @@
 import { supabase } from "./supabase.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  renderEntries();
+  addEventListeners();
+});
+
+async function renderEntries() {
+  const { data, error } = await supabase.from("problem_entry").select();
+
+  let problems = data.reverse();
+
+  try {
+    entriesList.innerHTML = "";
+    problems.forEach((p, index) => {
+      const li = document.createElement("li");
+      li.innerHTML = `<span>${p.title}</span><span>${p.date_solved}</span>`;
+      li.addEventListener("click", () => openDetail(problems, index));
+      entriesList.appendChild(li);
+    });
+  } catch {
+    console.log(error);
+  }
+}
+
+function addEventListeners() {
   const openFormBtn = document.getElementById("openFormBtn");
   const modal = document.getElementById("modal");
   const closeModal = document.getElementById("closeModal");
   const problemForm = document.getElementById("problemForm");
   const entriesList = document.getElementById("entriesList");
   const problemDetail = document.getElementById("problemDetail");
-  const { data, error } = await supabase.from("problem_entry").select();
-
-  let problems = data;
-
-  function renderEntries() {
-    try {
-      entriesList.innerHTML = "";
-      problems.forEach((p, index) => {
-        const li = document.createElement("li");
-        li.innerHTML = `<span>${p.title}</span><span>${p.date_solved}</span>`;
-        li.addEventListener("click", () => openDetail(index));
-        entriesList.appendChild(li);
-      });
-    } catch {
-      console.log(error);
-    }
-  }
+  const deleteBtn = document.getElementById("delete-btn");
 
   openFormBtn.addEventListener("click", () => {
     problemForm.classList.remove("hidden");
@@ -46,36 +53,42 @@ document.addEventListener("DOMContentLoaded", async () => {
       reflection: problemForm.reflection.value,
     };
 
-    console.log(entry)
-
     const { newProblem, error } = await supabase
       .from("problem_entry")
       .insert([entry])
       .select();
-    console.log(error);
     problems.unshift(newProblem);
-    renderEntries();
     problemForm.reset();
     modal.classList.add("hidden");
+    renderEntries();
   });
 
-  function openDetail(index) {
-    const p = problems[index];
-    problemForm.classList.add("hidden");
-    problemDetail.classList.remove("hidden");
+  deleteBtn.addEventListener("click", async () => {
+    const { response, error } = await supabase
+      .from("problem_entry")
+      .delete()
+      .eq("problem_id", document.getElementById("detailId").innerText);
+    modal.classList.add("hidden");
+    renderEntries();
+  });
+}
 
-    document.getElementById("detailTitle").innerText = p.title;
-    document.getElementById("detailPlatform").innerText = p.platform;
-    document.getElementById("detailDifficulty").innerText = p.difficulty;
-    document.getElementById("detailCategory").innerText = p.category;
-    document.getElementById("detailTime").innerText = p.time_taken_minutes;
-    document.getElementById("detailLanguage").innerText = p.language;
-    document.getElementById("detailDate").innerText = p.date_solved;
-    document.getElementById("detailSolution").innerText = p.solution_code;
-    document.getElementById("detailReflection").innerText = p.reflection;
+function openDetail(problems, index) {
+  const p = problems[index];
+  console.log(p);
+  problemForm.classList.add("hidden");
+  problemDetail.classList.remove("hidden");
 
-    modal.classList.remove("hidden");
-  }
+  document.getElementById("detailId").innerText = p.problem_id;
+  document.getElementById("detailTitle").innerText = p.title;
+  document.getElementById("detailPlatform").innerText = p.platform;
+  document.getElementById("detailDifficulty").innerText = p.difficulty;
+  document.getElementById("detailCategory").innerText = p.category;
+  document.getElementById("detailTime").innerText = p.time_taken_minutes;
+  document.getElementById("detailLanguage").innerText = p.language;
+  document.getElementById("detailDate").innerText = p.date_solved;
+  document.getElementById("detailSolution").innerText = p.solution_code;
+  document.getElementById("detailReflection").innerText = p.reflection;
 
-  renderEntries();
-});
+  modal.classList.remove("hidden");
+}
